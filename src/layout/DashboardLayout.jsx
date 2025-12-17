e620d4bcdd2738e87d4d7a26691d83bc4261ba6f
enestkt <121498839+enestkt@users.noreply.github.com>
2025-12-17 21:37:59 +0300

    fixed

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,19 +1,17 @@
+// src/layout/DashboardLayout.jsx
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-import "../styles/layout.css";
+import Sidebar from "../components/Sidebar";
 
 function DashboardLayout() {
     return (
         <div className="layout">
-            {/* Sol taraf sabit Sidebar */}
             <Sidebar />
 
-            {/* Sağ taraf Navbar + Değişen İçerik */}
             <div className="main">
                 <Navbar />
+
                 <main className="content">
-                    {/* Sayfalar (Dashboard, Books vb.) buraya gelir */}
                     <Outlet />
                 </main>
             </div>
